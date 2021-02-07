f59c7cf05a9d2da4860a5315dac56782d9052d47
Bayan Ahmadi <paimanahmadi101@gmail.com>
2021-02-07 06:07:58 -0500

    changed html rsp to emojis; created game method to decide winner; probaly gonna change result and game ..doesnt make sense lol

--- src/main/java/com/ahmadiba/assignment1/controller/RPSController.java
+++ src/main/java/com/ahmadiba/assignment1/controller/RPSController.java
@@ -1,9 +1,12 @@
 package com.ahmadiba.assignment1.controller;
 
+import com.ahmadiba.assignment1.domain.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RPSController {
@@ -14,5 +17,12 @@ public class RPSController {
     public String input(){
         logger.trace("input() is called");
         return "Input";
+    }
+
+    @GetMapping("/Output")
+    public ModelAndView output(@ModelAttribute Result result){
+        logger.trace("output() is called");
+        logger.debug("result = " + result);
+        return new ModelAndView("Output", "result", result);
     }
 }
--- src/main/java/com/ahmadiba/assignment1/domain/Result.java
+++ src/main/java/com/ahmadiba/assignment1/domain/Result.java
@@ -4,14 +4,59 @@ import java.io.Serializable;
 
 public class Result implements Serializable {
 
+    public int choice =1;
 
-    public int messege =1;
+    public Result(){
 
-    public int getMessege() {
-        return messege;
     }
 
-    public void setMessege(int messege) {
-        this.messege = messege;
+    public int getChoice() {
+        return choice;
     }
-}
+
+    public void setChoice(int choice) {
+        this.choice = choice;
+    }
+
+
+
+    public void winner(){
+
+        int comChoice = (int)(Math.random()* 3);
+
+        String game = "";
+
+
+
+        if(comChoice == choice){
+
+            game = "You tied";
+        }
+
+        else if (( choice == 1 && comChoice == 3) || ( choice == 3 && comChoice == 2) || ( choice == 2 && comChoice ==1)){
+
+            game = "You Win!";
+        }
+
+        else game = "You Lose";
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
