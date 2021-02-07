44b245a62b775cadac2fa45db0e0307b4f55f3a1
Bayan Ahmadi <paimanahmadi101@gmail.com>
2021-02-07 01:42:33 -0500

    converted html to thymeleaf and added controller and getmapping to controller class

--- src/main/java/com/ahmadiba/assignment1/controller/RPSController.java
+++ src/main/java/com/ahmadiba/assignment1/controller/RPSController.java
@@ -1,4 +1,18 @@
 package com.ahmadiba.assignment1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RPSController {
+
+    private final Logger logger = LoggerFactory.getLogger(RPSController.class);
+
+    @GetMapping(value={"/", "/Input"})
+    public String input(){
+        logger.trace("input() is called");
+        return "Input";
+    }
 }
---
+++ src/main/java/com/ahmadiba/assignment1/domain/Result.java
@@ -0,0 +1,6 @@
+package com.ahmadiba.assignment1.domain;
+
+import java.io.Serializable;
+
+public class Result implements Serializable {
+}
