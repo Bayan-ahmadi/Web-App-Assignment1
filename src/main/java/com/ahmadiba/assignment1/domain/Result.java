b05e50472c97d0f887b2e5beb3a3c1c16bc58baa
Bayan Ahmadi <paimanahmadi101@gmail.com>
2021-02-07 03:23:16 -0500

    generated get;set for messege value

--- src/main/java/com/ahmadiba/assignment1/domain/Result.java
+++ src/main/java/com/ahmadiba/assignment1/domain/Result.java
@@ -3,4 +3,15 @@ package com.ahmadiba.assignment1.domain;
 import java.io.Serializable;
 
 public class Result implements Serializable {
+
+
+    public int messege =1;
+
+    public int getMessege() {
+        return messege;
+    }
+
+    public void setMessege(int messege) {
+        this.messege = messege;
+    }
 }
